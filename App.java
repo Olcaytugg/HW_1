c364fa9cf249b7642c8922de5df55604b6f96575
Olcaytugg <157888828+Olcaytugg@users.noreply.github.com>
2024-03-12 10:55:22 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,26 @@
 public class App{
 
+  public static int compute(int a,int b,int[] nums1,int nums2[]){
+    //c ->multiply a with every element of nums1
+    //d ->multiply b with every element of nums2
+    //add c to d
+    int c = 0;
+    int d = 0;
+    for(int x = 0;x < nums1.length;x++){
+    c+=
+    
+    }
+    for(int y = 0;y < nums2.length;y++){
+    
+    
+    }
+    
+
+  }
+
+
+
+    
 
 
 }
